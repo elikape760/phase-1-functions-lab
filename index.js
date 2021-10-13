6f91922f871587be6c9c9e97c90107f00eb0da4a
Elias <elikapeluschnik@gmail.com>
2021-10-13 19:55:46 -0400

    commit tree 98c3c0811c13cf9de735772af88ef0e2202a6067

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street1) {
+    let hq = 42;
+   if (street1 > hq) {
+   return street1 - hq;
+    } else {
+        return hq - street1;
+    }
+}
+// distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(street2) {
+    // distanceFromHqInBlocks(street2);
+    let difference = distanceFromHqInBlocks(street2);
+    let distance = 264; 
+    return difference * distance;
+}
+
+function distanceTravelledInFeet(street, hq) {
+    if (street > hq) {
+        return (street - hq)*264;
+    } else {
+        return (hq - street)*264;
+    }
+}
+
+function calculatesFarePrice(street3, street4) {
+
+    
+    if (Math.abs(street3 - street4) * 264 <= 400) {
+    return 0;}
+     else if (Math.abs(street3 - street4) * 264< 1600) {
+    return Math.abs((street3 - street4) * 264 - 400) * .02
+    }
+
+    else if (Math.abs(street3 - street4) * 264 <= 2400) {
+        return 25
+    }
+    else if ((Math.abs(street3 - street4) * 264 - 400) >= 2100) {
+        return 'cannot travel that far'
+        }
+    }
